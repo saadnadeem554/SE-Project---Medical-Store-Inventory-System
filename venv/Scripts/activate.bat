cb76f6a44e13256c0eaf4c7605cb209cd1badebe
Amaar Khan <i220759@nu.edu.pk>
2025-03-22 15:28:44 +0500

    Fixed authentication bug and improved UI

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\SEM 6\SE\Project\SE-Project\venv
+set VIRTUAL_ENV=C:\Users\erkp\Desktop\SE-Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
